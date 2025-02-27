0551ede1d0e75c87ad66c72858aab56e8c7fa8ae
Jean Figeuroa <jbster559@gmail.com>
2025-02-26 19:24:43 -0800

    Add project files.

---
+++ C#FinalProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Gonna start with recreating my Pseudo code and go from there
+        }
+    }
+}
